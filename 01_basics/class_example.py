f55add85f8d8da6d9cd07f65c25e0ef79083fc9f
= <=>
2026-07-20 14:54:54 +0530

    refactor: encapsulate model attribute and add static method and property to Car class

--- 01_basics/class_example.py
+++ 01_basics/class_example.py
@@ -2,12 +2,20 @@ class Car:
     total_cars = 0
     def __init__(self,brand ,model,year):
         self.brand = brand
-        self.model = model
+        self.__model = model
         self.year = year
         Car.total_cars +=1
     
     def fullName(self):
-        return f"{self.brand} {self.model} {self.year}"
+        return f"{self.brand} {self.__model} {self.year}"
+
+    @staticmethod
+    def mystatic():
+        return "this is a static method"
+
+    @property
+    def model(self):
+        return self.__model
 
 class ElectricCar(Car):
     def __init__(self,brand,model,year,battery):
@@ -20,3 +28,6 @@ print(myECar.fullName())
 myCar = Car('Toyota', 'Camry', 2024)
 print(myCar.fullName())
 print(myCar.total_cars)
+print(myCar.mystatic())
+print(myCar.model)
+print(Car.mystatic())
