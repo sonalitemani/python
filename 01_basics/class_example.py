67821d64bfbc1bbdbd82f71c18684270aa233c3c
= <=>
2026-07-20 14:13:34 +0530

    feat: add total_cars class variable and ElectricCar subclass implementation

--- 01_basics/class_example.py
+++ 01_basics/class_example.py
@@ -1,11 +1,22 @@
 class Car:
+    total_cars = 0
     def __init__(self,brand ,model,year):
         self.brand = brand
         self.model = model
         self.year = year
+        Car.total_cars +=1
     
     def fullName(self):
         return f"{self.brand} {self.model} {self.year}"
 
+class ElectricCar(Car):
+    def __init__(self,brand,model,year,battery):
+        super().__init__(brand,model,year)
+        self.battery = battery
+
+myECar = ElectricCar('TTT','2222','2922','85KWH')
+print(myECar.fullName())
+
 myCar = Car('Toyota', 'Camry', 2024)
 print(myCar.fullName())
+print(myCar.total_cars)
