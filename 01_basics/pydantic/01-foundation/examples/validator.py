62cd033d3fe13342ba84a13f0f48ee78e0df614f
= <=>
2026-07-23 15:50:25 +0530

    feat: add BookingModel with Field validation and computed total_amount property

--- 01_basics/pydantic/01-foundation/examples/validator.py
+++ 01_basics/pydantic/01-foundation/examples/validator.py
@@ -1,4 +1,4 @@
-from pydantic import field_validator , model_validator ,computed_field ,BaseModel
+from pydantic import field_validator , model_validator ,computed_field ,BaseModel ,Field
 
 class User(BaseModel):
     username: str
@@ -33,7 +33,23 @@ class Product(BaseModel):
     @property
     def total_price(self) -> float:
         return self.price * self.quantity
-    
+
+
+class BookingModel(BaseModel):
+    user_id: int
+    room_id: int
+    nights: int = Field(...,gt=0)
+    rate_per_night: float 
+
+    @computed_field
+    @property
+    def total_amount(self) -> float:
+        return self.nights * self.rate_per_night
+      
+
+booking_1 = BookingModel(user_id=1 , room_id=101 , nights=3, rate_per_night=100.5)
+
+print(booking_1.total_amount)
 
 product = Product(price=10 , quantity=2)
 
